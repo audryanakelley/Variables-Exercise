ba428a0d58664c51c189c0a4abc59b876da5989f
Audryana Kelley <audryanakelley@gmail.com>
2024-07-08 20:37:41 -0500

    Declared and Initialized varaibles and Interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           // Variables
+           
+           //Number types 
+           
+           //Declaring a variable ( create a variable0
+           
+           //Camel case - everyNextWordStartsWithCapital
+           
+           int wholeNumbers = 10;
+           
+           //initializing a variable (giving it a value)
+
+           wholeNumbers = 15;
+
+           //Or do Both
+           //Declaring and initializing
+
+           double average = 99.99;
+
+           decimal currency = 1000.00m;
+           
+           //Word types
+
+           string ourClass = "CSHARP49";
+
+           char KEY = '1'; 
+           
+           //Fact type
+
+           bool isCoding = true;
+           
+           //String Interpolation 
+
+           String dogName = "Max"; 
+           int dogAge = 10;
+
+           Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old"); 
+
+
+
+
+
         }
     }
 }
